92bdb899c4f565680bb0adc0508f7debb417d9b0
tucci <tuccisteven@gmail.com>
2016-02-20 15:02:20 -0800

    added comments

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
+var twilio = require("twilio");
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// Webhook for twilio
+app.post('/sms', twilio.webhook(), function(request, response) {
+	// Get message
+	var twilio_msg = request.body.Body;
+	// Parse message
+
+	// Call tripadvisior api
+
+	// get information from trip advisor
+
+	// parse info into the text message
+
+	// send text message
 });
+
+app.listen(3000, function () {
+  
+});
+
