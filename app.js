601450174e28bca0ada8bf47be672dbeca4d9548
Johnny Mak <johnny_mak@outlook.com>
2016-02-21 07:24:20 -0500

    Fakin Require Request rip me. Functions Done.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 //var APPLICATION_SID = 'PN85796b5ebbeb36f057ff3f7d5332f980';
-var ACCOUNT_SID = 'AC336fb1664da1a7d3de5cbcf4bbe5ba3a';
-var AUTH_TOKEN = '30b201bfe0053ca9884a9c891463c871';
-var TWILIO_PHONE = '+14387937806';
-var TEST_PHONE = '+15142913224';
+var ACCOUNT_SID = 'AC332a4111136723617783087ad398967d';
+var AUTH_TOKEN = '2fd2fe7642db69b49ab773a30feb950a';
+var TWILIO_PHONE = '+14387937502';
+var TEST_PHONE = '+15149534295';
+
+var NOMINATIM_KEY   = "IsgDCmUVcbopSLN9PI1GGAeGDmNAnZrT";
+var TRIPADVISOR_KEY = "6DB6633295E242028B9237D14411A773";
+
+
 
 var express = require('express'),
     bodyParser = require('body-parser'),
     http = require('http'),
     path = require('path'),
-    twilio = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
+    twilio = require('twilio')(ACCOUNT_SID, AUTH_TOKEN),
+    request = require("request");
 
-var tripAdvisor = require('tripadv');
+//var tripAdvisor = require('tripadv');
 
 var app = express();
 
@@ -21,19 +27,161 @@ app.use(bodyParser.urlencoded({
 // Twilio request authentication with custom URL
 app.post('/sms', function(req, res) {
 	var message = req.body.Body;
-    twilio.sendMessage({
-	    to: TEST_PHONE, 
-	    from: TWILIO_PHONE, 
-	    body: message
-	}, function(err, responseData) { 
 
-	    if (!err) {
-	        console.log(responseData.from); 
-	        console.log(responseData.body); 
-	    }
-	});
-	res.type('text/xml');
-	res.send(req.body.Body);
+
+
+    // COMMAND PARSER
+    var evalString = message.toLowerCase(); //"Find me pizza Near 6644 Fielding In Montreal".toLowerCase();
+    //var evalString = "Find me 5801891".toLowerCase();
+    // var evalString = "HeLP!!!!!".toLowerCase();
+
+    //var evalString = "HElp!".toLowerCase();
+    if(evalString.search("help") != -1) {
+        console.log("SEND HELP INFO");
+    }
+
+    else if(evalString.search("near") == -1) {
+        var evalLoc = evalString.match("me (.*)");
+        var locationID = evalLoc[1].split(" ");
+
+        var outputMessage = "";
+
+    
+        url = "http://api.tripadvisor.com/api/partner/2.0/location/" + locationID + "?key=" + TRIPADVISOR_KEY;
+        //console.log(url);
+
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+
+
+        if (!error && response.statusCode === 200) {
+
+            var obj = JSON.stringify(body);
+            var parsedData = JSON.parse(obj);
+            outputMessage = parsedData.name + " is located at " + parsedData.address_obj.street1;
+
+            // ADD SENDING TEXT PART
+
+            twilio.sendMessage({
+		    to: TEST_PHONE, 
+		    from: TWILIO_PHONE, 
+		    body: outputMessage
+			}, function(err, responseData) { 
+
+			    if (!err) {
+			        console.log(responseData.from); 
+			        console.log(responseData.body); 
+			    }
+			});
+
+			res.type('text/xml');
+			res.send(req.body.Body);
+        }
+
+        });
+
+    }
+
+    else {
+        var searchTerm = evalString.match("me (.*) near");
+        var address    = evalString.match("near (.*) in").toString().split(" ").join('+').split(",");
+        var city       = evalString.match("in (.*)").toString().split(" ").join('+').split(",");
+        var location   = address[1] + '+' + city[1];
+
+        // console.log(location);
+
+        var searchLocationType = "restaurants";
+
+        // var text = getLocalSearch(location[1], searchTerm[1], "restaurants",  NOMINATIM_KEY, TRIPADVISOR_KEY);
+        // console.log(text);
+
+        var url = 'http://open.mapquestapi.com/nominatim/v1/search.php?key=' + NOMINATIM_KEY + '&format=json&q=' + location; 
+        //console.log(url);
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+                var obj = JSON.stringify(body);
+                var parsedData = JSON.parse(obj);
+                var latLon = parsedData[0].lat + "," + parsedData[0].lon;
+                var outputMessage = "";
+                // console.log(latLon);
+
+                // get information from trip advisor
+                url = "https://api.tripadvisor.com/api/partner/2.0/search/" + searchTerm[1] + "?key=" + TRIPADVISOR_KEY + "&map=" + latLon + "&category=" + searchLocationType;
+                console.log(url);
+                request({
+                    url: url,
+                    json: true
+                }, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+
+                    var obj = JSON.stringify(body);
+                    var parsedData = JSON.parse(obj);
+                    var evalStatement = 'outputMessage += (i + 1) +  "- " + parsedData.' + searchLocationType + '[i].name + " (ID: " + parsedData.' + searchLocationType + '[i].location_id + ") ";'
+
+                    for(i = 0; i < 3; i++) {
+                        //outputMessage += (i + 1) +  "- " + parsedData.restaurants[i].name + " (ID: " + parsedData.restaurants[i].location_id + ") ";
+                        eval(evalStatement);
+                    }
+
+                    outputMessage += 'TextBack: "LookUp + ID" to get ' + searchLocationType + ' address';     
+                    console.log(outputMessage);  
+                   
+                    
+
+                    // ADD SENDING TEXT PART
+
+                    twilio.sendMessage({
+                    to: TEST_PHONE, 
+                    from: TWILIO_PHONE, 
+                    body: outputMessage
+                    }, function(err, responseData) { 
+
+                        if (!err) {
+                            console.log(responseData.from); 
+                            console.log(responseData.body); 
+                        }
+                    });
+
+                    res.type('text/xml');
+                    res.send(req.body.Body);
+
+
+                }
+
+                });
+            }
+
+
+        });
+     
+
+    }
+
+
+
+
+
+
+ //    twilio.sendMessage({
+	//     to: TEST_PHONE, 
+	//     from: TWILIO_PHONE, 
+	//     body: message
+	// }, function(err, responseData) { 
+
+	//     if (!err) {
+	//         console.log(responseData.from); 
+	//         console.log(responseData.body); 
+	//     }
+	// });
+	// res.type('text/xml');
+	// res.send(req.body.Body);
 });
 
 // Start an HTTP server with this Express app
--- tripadv.js
+++ tripadv.js
@@ -4,8 +4,8 @@ var app = express();
 var request = require("request");
 
 
-var nominatimKey   = "IsgDCmUVcbopSLN9PI1GGAeGDmNAnZrT";
-var tripAdvisorKey = "6DB6633295E242028B9237D14411A773";
+var NOMINATIM_KEY   = "IsgDCmUVcbopSLN9PI1GGAeGDmNAnZrT";
+var TRIPADVISOR_KEY = "6DB6633295E242028B9237D14411A773";
 
 
 
@@ -24,77 +24,211 @@ var tripAdvisorKey = "6DB6633295E242028B9237D14411A773";
 
 //})
 
-    var location = "jarry+montreal";    // PASS TROUGHT USER SEARCH
-    var searchTerm = "pizza";
-    var searchLocationType = 'restaurants';
-    var returnMessage = ""; 
+    // var location = "jarry+montreal";    // PASS TROUGHT USER SEARCH
+    // var searchTerm = "pizza";
+    // var searchLocationType = 'restaurants';
 
 
-    // SEARCH FOR PLACES BASED ON INPUTED LOCATION
-    // parse info into the text message
-    var url = 'http://open.mapquestapi.com/nominatim/v1/search.php?key=' + nominatimKey + '&format=json&q=' + location; 
-    console.log(url);
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-
-        if (!error && response.statusCode === 200) {
-            var obj = JSON.stringify(body);
-            var parsedData = JSON.parse(obj);
-            var latLon = parsedData[0].lat + "," + parsedData[0].lon;
-
-            // get information from trip advisor
-            url = "https://api.tripadvisor.com/api/partner/2.0/search/" + searchTerm + "?key=" + tripAdvisorKey + "&map=" + latLon + "&category=" + searchLocationType;
-
-            request({
-                url: url,
-                json: true
-            }, function (error, response, body) {
-
-            if (!error && response.statusCode === 200) {
-
-                var obj = JSON.stringify(body);
-                var parsedData = JSON.parse(obj);
-                var evalStatement = 'returnMessage += (i + 1) +  "- " + parsedData.' + searchLocationType + '[i].name + " (ID: " + parsedData.' + searchLocationType + '[i].location_id + ") ";'
-                var tempText
-
-                for(i = 0; i < 3; i++) {
-                    //returnMessage += (i + 1) +  "- " + parsedData.restaurants[i].name + " (ID: " + parsedData.restaurants[i].location_id + ") ";
-                    eval(evalStatement);
-                }
-
-                returnMessage += 'TextBack: "LookUp + ID" to get ' + searchLocationType + ' address';
-
-                console.log(returnMessage);        
-            }
-
-            });
-        }
-
-    });
+    // function getLocalSearch(location, searchTerm, searchLocationType, NOMINATIM_KEY , TRIPADVISOR_KEY) {
 
 
-    // LOOKUP TO SEARCH A LOCATION BASED ON PROVIDED ID
-    // parse info into the text message
-    var locationID = 5801891;
-    url = "http://api.tripadvisor.com/api/partner/2.0/location/" + locationID + "?key=" + tripAdvisorKey;
+    //     // SEARCH FOR PLACES BASED ON INPUTED LOCATION
+    //     // parse info into the text message
+    //     var url = 'http://open.mapquestapi.com/nominatim/v1/search.php?key=' + NOMINATIM_KEY + '&format=json&q=' + location; 
+    //     //console.log(url);
+    //     request({
+    //         url: url,
+    //         json: true
+    //     }, function (error, response, body) {
 
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
+    //         if (!error && response.statusCode === 200) {
+    //             var obj = JSON.stringify(body);
+    //             var parsedData = JSON.parse(obj);
+    //             var latLon = parsedData[0].lat + "," + parsedData[0].lon;
+    //             // console.log(latLon);
 
-    if (!error && response.statusCode === 200) {
+    //             // get information from trip advisor
+    //             url = "https://api.tripadvisor.com/api/partner/2.0/search/" + searchTerm + "?key=" + TRIPADVISOR_KEY + "&map=" + latLon + "&category=" + searchLocationType;
+    //             //console.log(url);
+    //             request({
+    //                 url: url,
+    //                 json: true
+    //             }, function (error, response, body) {
 
-        var obj = JSON.stringify(body);
-        var parsedData = JSON.parse(obj);
-        returnMessage = parsedData.name + " is located at " + parsedData.address_obj.street1;
-        console.log(returnMessage);        
+    //             if (!error && response.statusCode === 200) {
+
+    //                 var obj = JSON.stringify(body);
+    //                 var parsedData = JSON.parse(obj);
+    //                 var evalStatement = 'outputMessage += (i + 1) +  "- " + parsedData.' + searchLocationType + '[i].name + " (ID: " + parsedData.' + searchLocationType + '[i].location_id + ") ";'
+
+    //                 for(i = 0; i < 3; i++) {
+    //                     //outputMessage += (i + 1) +  "- " + parsedData.restaurants[i].name + " (ID: " + parsedData.restaurants[i].location_id + ") ";
+    //                     eval(evalStatement);
+    //                 }
+
+    //                 outputMessage += 'TextBack: "LookUp + ID" to get ' + searchLocationType + ' address';     
+    //                 // console.log("OUTPUT: " + outputMessage);  
+    //                 //return outputMessage;
+    //             }
+
+    //             });
+    //         }
+
+
+    //     });
+
+    //     return outputMessage;
+    // }
+    
+
+    // function getAddressOnLocationID(locationID, TRIPADVISOR_KEY) {
+    
+    //     // LOOKUP TO SEARCH A LOCATION BASED ON PROVIDED ID
+    //     // parse info into the text message
+    //     var outputMessage = "text"; 
+
+    //     url = "http://api.tripadvisor.com/api/partner/2.0/location/" + locationID + "?key=" + TRIPADVISOR_KEY;
+    //     console.log(url);
+
+    //     request({
+    //         url: url,
+    //         json: true
+    //     }, function (error, response, body) {
+
+    //     if (!error && response.statusCode === 200) {
+
+    //         var obj = JSON.stringify(body);
+    //         var parsedData = JSON.parse(obj);
+    //         outputMessage = parsedData.name + " is located at " + parsedData.address_obj.street1;
+    //         //console.log(outputMessage);
+    //     }
+
+    //     });
+
+    //     return outputMessage;
+    // }
+
+
+
+    /***
+    
+        THIS PART GOES INTO APP.JS
+
+    ***/
+
+
+    // COMMAND PARSER
+    var evalString = "Find me pizza Near 6644 Fielding In Montreal".toLowerCase();
+    //var evalString = "Find me 5801891".toLowerCase();
+    // var evalString = "HeLP!!!!!".toLowerCase();
+
+    //var evalString = "HElp!".toLowerCase();
+    if(evalString.search("help") != -1) {
+        console.log("SEND HELP INFO");
     }
 
-    });
+    else if(evalString.search("near") == -1) {
+        var evalLoc = evalString.match("me (.*)");
+        var locationID = evalLoc[1].split(" ");
+        //console.log("="+locationID+"+");
+        // console.log(getAddressOnLocationID(locationID[1], TRIPADVISOR_KEY));
 
+        var outputMessage = "";
+
+    
+        url = "http://api.tripadvisor.com/api/partner/2.0/location/" + locationID + "?key=" + TRIPADVISOR_KEY;
+        //console.log(url);
+
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+
+
+        if (!error && response.statusCode === 200) {
+
+            var obj = JSON.stringify(body);
+            var parsedData = JSON.parse(obj);
+            outputMessage = parsedData.name + " is located at " + parsedData.address_obj.street1;
+            console.log(outputMessage);
+            // ADD SENDING TEXT PART
+
+            
+        }
+
+        });
+
+    }
+
+    else {
+        var searchTerm = evalString.match("me (.*) near");
+        var address    = evalString.match("near (.*) in").toString().split(" ").join('+').split(",");
+        var city       = evalString.match("in (.*)").toString().split(" ").join('+').split(",");
+        var location   = address[1] + '+' + city[1];
+
+        // console.log(location);
+
+        var searchLocationType = "restaurants";
+
+        // var text = getLocalSearch(location[1], searchTerm[1], "restaurants",  NOMINATIM_KEY, TRIPADVISOR_KEY);
+        // console.log(text);
+
+        var url = 'http://open.mapquestapi.com/nominatim/v1/search.php?key=' + NOMINATIM_KEY + '&format=json&q=' + location; 
+        //console.log(url);
+        request({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+                var obj = JSON.stringify(body);
+                var parsedData = JSON.parse(obj);
+                var latLon = parsedData[0].lat + "," + parsedData[0].lon;
+                var outputMessage = "";
+                // console.log(latLon);
+
+                // get information from trip advisor
+                url = "https://api.tripadvisor.com/api/partner/2.0/search/" + searchTerm[1] + "?key=" + TRIPADVISOR_KEY + "&map=" + latLon + "&category=" + searchLocationType;
+                console.log(url);
+                request({
+                    url: url,
+                    json: true
+                }, function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+
+                    var obj = JSON.stringify(body);
+                    var parsedData = JSON.parse(obj);
+                    var evalStatement = 'outputMessage += (i + 1) +  "- " + parsedData.' + searchLocationType + '[i].name + " (ID: " + parsedData.' + searchLocationType + '[i].location_id + ") ";'
+
+                    for(i = 0; i < 3; i++) {
+                        //outputMessage += (i + 1) +  "- " + parsedData.restaurants[i].name + " (ID: " + parsedData.restaurants[i].location_id + ") ";
+                        eval(evalStatement);
+                    }
+
+                    outputMessage += 'TextBack: "LookUp + ID" to get ' + searchLocationType + ' address';     
+                    console.log(outputMessage);  
+                   
+                    
+
+                    // ADD SENDING TEXT PART
+
+
+                }
+
+                });
+            }
+
+
+        });
+     
+
+    }
+
+
+    
+
+    
     
 
     
@@ -102,7 +236,7 @@ var tripAdvisorKey = "6DB6633295E242028B9237D14411A773";
     // send text message
 
 
-app.listen(3000, function () {
+// app.listen(3000, function () {
   
-});
+// });
 
