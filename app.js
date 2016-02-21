6dd685ca3ed4fea54aaef17a0fbafe2a7a151fff
tucci <tuccisteven@gmail.com>
2016-02-20 22:12:56 -0800

    please...

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
-var express = require('express');
+var APPLICATION_SID = 'PN85796b5ebbeb36f057ff3f7d5332f980';
+var ACCOUNT_SID = 'AC332a4111136723617783087ad398967d';
+var AUTH_TOKEN = '2fd2fe7642db69b49ab773a30feb950a';
+var TWILIO_PHONE = '+14387937502';
+var TEST_PHONE = '+15149534295';
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    path = require('path'),
+    twilio = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
+
 var app = express();
-var twilio = require("twilio");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Twilio request authentication with custom URL
+app.post('/sms', function(req, res) {
+	 var options = { url: 'http://smsme2.us-east-1.elasticbeanstalk.com/sms'};
+    if (twilio.validateExpressRequest(req, AUTH_TOKEN, options)) {
+        client.sendMessage({
+		    to: TEST_PHONE, 
+		    from: TWILIO_PHONE, 
+		    body: "Hello World " + req.Body 
+		}, function(err, responseData) { 
+
+		    if (!err) {
+		        console.log(responseData.from); 
+		        console.log(responseData.body); 
+		    }
+		});
+		
+		res.type('text/xml');
+		res.send('Something worked!!');
+    } else {
+        res.status(403).send('you are not twilio. Buzz off.');
+    }
 });
 
-// Webhook for twilio
-app.post('/sms', twilio.webhook(), function(request, response) {
-	// Get message
-	var twilio_msg = request.body.Body;
-	// Parse message
-
-	// Call tripadvisior api
-
-	// get information from trip advisor
-
-	// parse info into the text message
-
-	// send text message
-});
-
-app.listen(3000, function () {
-  
-});
-
+// Start an HTTP server with this Express app
+app.listen(process.env.PORT || 3000);
